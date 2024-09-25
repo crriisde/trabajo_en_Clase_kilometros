d188088323c5ce00051008cfd25daf4095c42e55
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-24 20:09:17 -0600

    Add files via upload

---
+++ kilometros_salas/kilometros.py
@@ -0,0 +1,16 @@
+print (" ") #esta variable muestra las comillas para el texto
+print ("Salas De La O Cristian David : Mi practica de clase string.py") #esta variable mostrara en la pantalla los datos 
+#de la practica
+print (" ") #esta variable muestra las comillas para el texto 
+
+def kilometros_litros(a, b): #esta linea define las variables a usar
+    return (a, b) #esta linea concluye la ejecucion
+
+a = int(input('Pon la cantidad de kilometros recorridos: ')) #esta linea pide ingresar los kilometros
+b = int(input("Pon la cantidad de litros usados: ")) #esta linea pide ingresar los litros
+
+resultado = (a / b) #esta linea calcula el resultado 
+print ("la cantidad de litros usados por kilometro es: ") #esta linea muestra el resultado
+print (resultado) #esta linea muestra el resultado
+
+print (" ") #esta variable muestra las comillas para el texto 
